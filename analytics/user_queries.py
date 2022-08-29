2538fcb82ec6bd0e2e6900894d5625b9ebb1ae95
vishalsingh1080 <vishalsingh1080@gmail.com>
2022-08-29 19:59:04 +0530

    queries update

--- analytics/user_queries.py
+++ analytics/user_queries.py
@@ -23,10 +23,24 @@ user_queries = {
                                 100 as 'Total Signups EMAIL (%)' \
                                 NULL and is_sso=0) (select count(*) from access_user) * \
                                 100 as 'Total Signups EMAIL (%)'",
-    "Monthly Signups": "SELECT LEFT(created_at, 7) AS Month, \
-                             COUNT(id) AS 'Monthly signups' \
-                             FROM access_user \
-                            GROUP BY LEFT(created_at, 7)",
+    "Total signups count": "SELECT count(if(is_sso = 1,1,null)) as SSO_count, \
+                                   count(case when email is not null then 1 else null end) as email_count, \
+                                   count(case when phone is not null then 1 else null end) as phone_count \
+                            FROM access_user",
+    "Monthly Signups count": "SELECT MONTH(created_at), \
+                                     count(if(is_sso = 1,1,null)) as SSO_count, \
+                                     count(case when email is not null then 1 else null end) as email_count, \
+                                     count(case when phone is not null then 1 else null end) as phone_count \
+                              FROM access_user \
+                              GROUP BY MONTH(created_at) \
+                              ORDER BY MONTH(created_at)",
+     "Weekly signups count": "SELECT WEEK(created_at), \
+                                     count(if(is_sso = 1,1,null)) as SSO_count, \
+                                     count(case when email is not null then 1 else null end) as email_count, \
+                                     count(case when phone is not null then 1 else null end) as phone_count \
+                              FROM access_user \
+                              GROUP BY WEEK(created_at) \
+                              ORDER BY WEEK(created_at) DESC", 
     "Daily Signups":"SELECT DATE(created_at) AS DAY, \
                           COUNT(id) AS 'Daily signups' \
                           FROM access_user \
@@ -112,29 +126,29 @@ user_queries = {
 				                FROM access_user au \
 				                JOIN utils_userinputs uu ON au.id = uu.user_id_id)",                                                                                                                                                     
 }
-repeat_users = {
-    "Total template usage": "SELECT count(template_name) AS total_template_name, \
-                            template_name FROM utils_userinputs GROUP BY \
-                            template_name ORDER BY total_template_name",
-    "Users Email": "select access_user.email as email, \
-                    first_name, last_name, date_format(utils_userinputs.created_at, \
-                   '%Y-%m-%d') as ran_at, template_name \
-                    from access_user cross join \
-                    utils_userinputs on access_user.id = utils_userinputs.user_id_id \
-                    where email NOT IN ('vishalsingh1080@gmail.com', \
-                    'vishalkonline@gmail.com','vishal25797.work@gmail.com', \
-                    'vishal@yamak.ai', 'vipuldhiman24@gmail.com', \
-                    'srashti.ss03@gmail.com', 'shivammaddhesia0419@gmail.com', \
-                    'saurav.suman.319@gmail.com', 'rishabhb932@gmail.com', 'rasick@gmail.com', \
-                    'preethiknot1998@gmail.com', 'preethi@yamak.ai', 'bhardwaj.1@iitj.ac.in', \
-                    'admin@yamak.ai', 'abhinav.rai.1996@gmail.com', \
-                    'me.2019.bktaksande@bitwardha.ac.in', \
-                    'srashti.1531163@kiet.edu', 'srashti1163@gmail.com') \
-                    ORDER BY email, ran_at",
-    "Users Phone": "select access_user.phone as phone, access_user.email as email, \
-                    first_name, last_name, date_format(utils_userinputs.created_at, \
-                    '%Y-%m-%d') as ran_at, template_name from access_user cross join \
-                    utils_userinputs on access_user.id = utils_userinputs.user_id_id \
-                    where phone NOT IN ('8920564682', '9884209831', '9359489538', \
-                    '7899422725', '7042857001', '') ORDER BY phone, ran_at"
-}
+# repeat_users = {
+#     "Total template usage": "SELECT count(template_name) AS total_template_name, \
+#                             template_name FROM utils_userinputs GROUP BY \
+#                             template_name ORDER BY total_template_name",
+#     "Users Email": "select access_user.email as email, \
+#                     first_name, last_name, date_format(utils_userinputs.created_at, \
+#                    '%Y-%m-%d') as ran_at, template_name \
+#                     from access_user cross join \
+#                     utils_userinputs on access_user.id = utils_userinputs.user_id_id \
+#                     where email NOT IN ('vishalsingh1080@gmail.com', \
+#                     'vishalkonline@gmail.com','vishal25797.work@gmail.com', \
+#                     'vishal@yamak.ai', 'vipuldhiman24@gmail.com', \
+#                     'srashti.ss03@gmail.com', 'shivammaddhesia0419@gmail.com', \
+#                     'saurav.suman.319@gmail.com', 'rishabhb932@gmail.com', 'rasick@gmail.com', \
+#                     'preethiknot1998@gmail.com', 'preethi@yamak.ai', 'bhardwaj.1@iitj.ac.in', \
+#                     'admin@yamak.ai', 'abhinav.rai.1996@gmail.com', \
+#                     'me.2019.bktaksande@bitwardha.ac.in', \
+#                     'srashti.1531163@kiet.edu', 'srashti1163@gmail.com') \
+#                     ORDER BY email, ran_at",
+#     "Users Phone": "select access_user.phone as phone, access_user.email as email, \
+#                     first_name, last_name, date_format(utils_userinputs.created_at, \
+#                     '%Y-%m-%d') as ran_at, template_name from access_user cross join \
+#                     utils_userinputs on access_user.id = utils_userinputs.user_id_id \
+#                     where phone NOT IN ('8920564682', '9884209831', '9359489538', \
+#                     '7899422725', '7042857001', '') ORDER BY phone, ran_at"
+# }
