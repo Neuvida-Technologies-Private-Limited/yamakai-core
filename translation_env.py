e9d3414d1a029b9846d0a981845559aa11f3acc2
vishalsingh1080 <vishalsingh1080@gmail.com>
2022-09-21 17:42:35 +0530

    tested google v3

--- translation_env.py
+++ translation_env.py
@@ -1,8 +1,10 @@
 import os
  
 from google.cloud import translate_v2
+from google.cloud import translate
  
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"/home/vishal/Desktop/yamakai-core/hindi-eng-357711-94d6f15b00c1.json"
  
 translate_client = translate_v2.Client()
 
+client = translate.TranslationServiceClient()
