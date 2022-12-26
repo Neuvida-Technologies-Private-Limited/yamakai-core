b79362b5bdb1855301b5c8b7cf2973b4084a3dca
PUSHKAR RAJ <pushkar.raj6@gmail.com>
2022-12-26 19:52:33 +0530

    completely revamped

--- env.example.py
+++
@@ -1,5 +0,0 @@
-# Params required for utils
-host="<host>"
-username="<username>"
-password="<password>"
-db_name="<db_name>"
--- env.py
+++
@@ -1,5 +0,0 @@
-# Params required for utils
-host = "database-free-dev.cqdonlctukeo.ap-south-1.rds.amazonaws.com"
-username = "admin"
-password = "qtD8FErYcLYTaQypzBNz"
-db_name = "prod"
